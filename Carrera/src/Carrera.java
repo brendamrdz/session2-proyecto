a0e6c04f228f18e7ee0c71f75b471ef76cd92743
brendamrdz <brenda.mrdz@hotmail.com>
2023-07-20 00:45:08 -0600

    Add files via upload

---
+++ Carrera/src/Carrera.java
@@ -0,0 +1,52 @@
+
+public class Carrera {
+    
+    Integer distancia;
+    Integer ronda;
+    Long fecha;
+    public static void main(String[] args) throws Exception {
+        Carrera carrera1 = new Carrera(2,30,16106129672L);
+        System.out.println("La distancia es:"+carrera1.calcular_distancia()+" y el valor es de tipo "+((Object)carrera1.calcular_distancia()).getClass().getSimpleName());
+        
+        Nayarit nayarit1 = new Nayarit(carrera1.distancia, carrera1.ronda, carrera1.fecha);
+        nayarit1.setPersonas(16);
+        System.out.println( "\nRaíz cuadrada de participantes: " + nayarit1.raizCuadrada());
+        
+        Cuba cuba1 = new Cuba(carrera1.distancia, carrera1.ronda, carrera1.fecha);
+        cuba1.setNombreCorredor("Nombre");
+        System.out.println( "\nPrimer Letra del nombre: " + cuba1.primerLetra());
+        System.out.println( "\nTercera Letra del nombre: " + cuba1.tercerLetra());
+    }
+
+    public Carrera(Integer distancia, Integer ronda,Long fecha) {
+        this.distancia = distancia;
+        this.ronda = ronda;
+        this.fecha = fecha;
+    }
+
+    public Integer getDistancia() {
+        return distancia;
+    }
+    public void setDistancia(Integer distancia) {
+        this.distancia = distancia;
+    }
+
+    public Integer getRonda() {
+        return ronda;
+    }
+    public void setRonda(Integer ronda) {
+        this.ronda = ronda;
+    }
+    public Long getFecha() {
+        return fecha;
+    }
+    public void setFecha(Long fecha) {
+        this.fecha = fecha;
+    }
+
+    public String calcular_distancia(){
+        Integer dist = getDistancia() * getRonda();
+        String num_cadena = Integer.toString(dist);
+        return num_cadena;
+    }
+}
